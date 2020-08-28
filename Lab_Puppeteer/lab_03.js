96215300619a26635068b99ff830dd345455917c
Chien <wolfgang.chien@gmail.com>
2020-08-29 01:10:16 +0800

    Update Lab_Puppeteer, simulate mouse click

--- Lab_Puppeteer/lab_03.js
+++ Lab_Puppeteer/lab_03.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
-function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
-}
+// function delay(time) {
+//     return new Promise(function(resolve) { 
+//         setTimeout(resolve, time)
+//     });
+// }
 
 async function main () {
     const browser = await puppeteer.launch({headless: false});
@@ -12,12 +12,13 @@ async function main () {
     await page.goto('https://en.wikipedia.org/wiki/Wiki', {waitUntil: 'networkidle2'});
     await page.focus("#searchInput");
     await page.keyboard.type('earth');
-    await delay(1000);
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('\n');
-    await delay(1000);
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
+    (await page.$("#searchButton")).click();
+    // await delay(1000);
+    // await page.keyboard.press('ArrowDown');
+    // await page.keyboard.press('\n');
+    // await delay(1000);
+    // await page.screenshot({path: 'example.png'});
+    // await browser.close();
 }
 
 main();
